6b58c66ade549e1aaf261c7209eadae1a2cb8d86
Duc Harper <djharper757@gmail.com>
2026-09-04 00:53:42 -0400

    Reorganized main to more clearly compare Monte Carlo results with Black-Scholes equation

---
+++ include/StressTests.h
@@ -0,0 +1,8 @@
+//
+// Created by djhar on 9/4/2026.
+//
+
+#ifndef FOR_MONTE_STRESSTESTS_H
+#define FOR_MONTE_STRESSTESTS_H
+
+#endif //FOR_MONTE_STRESSTESTS_H
--- src/main.cpp
+++ src/main.cpp
@@ -20,5 +20,30 @@ int main() {
     MonteCarloEngine mcEngine(spotPrice, riskFreeRate, volatility, maturity);
 
     // Black-Scholes
-    
+    double bsCallPrice = pricer.price(call, spotPrice, riskFreeRate, volatility);
+    double bsPutPrice  = pricer.price(put, spotPrice, riskFreeRate, volatility);
+
+    std::cout << "Black-Scholes Call Price: " << bsCallPrice << "\n";
+    std::cout << "Black-Scholes Put Price:  " << bsPutPrice << "\n";
+
+    // Monte Carlo Engine
+    PricingResult mcCallPrice = mcEngine.price(call, 100000);
+    PricingResult mcPutPrice = mcEngine.price(put, 100000);
+
+    // Comparing Black-Scholes and Monte Carlo Engine
+    std::cout << "Monte Carlo Call Price:   " << mcCallPrice.price
+              << " (BS diff: " << std::abs(mcCallPrice.price - bsCallPrice) << ")\n";
+
+    std::cout << "Monte Carlo Put Price:    " << mcPutPrice.price
+              << " (BS diff: " << std::abs(mcPutPrice.price - bsPutPrice) << ")\n";
+
+    // Greeks
+    std::cout << "\nCall Greeks:\n";
+    std::cout << "Delta: " << pricer.delta(call, spotPrice, riskFreeRate, volatility) << "\n";
+    std::cout << "Gamma: " << pricer.gamma(call, spotPrice, riskFreeRate, volatility) << "\n";
+    std::cout << "Vega:  " << pricer.vega(call, spotPrice, riskFreeRate, volatility) << "\n";
+    std::cout << "Theta: " << pricer.theta(call, spotPrice, riskFreeRate, volatility) << "\n";
+    std::cout << "Rho:   " << pricer.rho(call, spotPrice, riskFreeRate, volatility) << "\n";
+
+    return 0;
 }
